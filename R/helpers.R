04e690841b689eca61c7b134fb866da95a44f87f
Dennis Duffy <dennis.duffy@state.mn.us>
2024-12-05 11:09:46 -0600

    fixed markdown template problem

--- R/helpers.R
+++ R/helpers.R
@@ -15,9 +15,20 @@ county_check <- function(file_check, dest_dir){
 
   markdown_path <- system.file("rmarkdown/templates/county_template/skeleton", "skeleton.Rmd", package = "elecchecks")
 
+  dbname <- keyring::key_get("warehouse_name")
+  host <- keyring::key_get("warehouse_host")
+  port <- keyring::key_get("warehouse_port")
+  user <- keyring::key_get("warehouse_user")
+  password <- keyring::key_get("warehouse_password")
+
   rmarkdown::render(markdown_path,
                     params = list(
-                      file_check = file_check
+                      file_check = file_check,
+                      dbname = dbname,
+                      host = host,
+                      port = port,
+                      user = user,
+                      password = password
                     ),
                     output_file = "REIS County Electricity Sales.html",
                     output_dir = dest_dir)
