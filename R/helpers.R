62acdf2836da0c5cf00a1b1bcdee93577e148fca
Dennis Duffy <dennis.duffy@state.mn.us>
2024-12-05 15:10:06 -0600

    added valid ID check

--- R/helpers.R
+++ R/helpers.R
@@ -22,3 +22,27 @@ valid_name_check <- function(data_check, utility_table){
   DT::datatable(u_names)
 
 }
+
+#' Utility ID check
+#'
+#' @param data_check A dataset to check
+#' @param utility_table A list of valid utility IDs
+#'
+#' @return A table of non-valid utility IDs
+#' @export
+#'
+#' @examples
+#' dir <- system.file("extdata", package = "elecchecks")
+#' file_check <- readxl::read_xlsx(paste(dir, "electric_class.xlsx", sep = "/"))
+#' id_table <- tibble::tribble(~utility_id, "some_id")
+#' valid_name_check(file_check, id_table)
+valid_id_check <- function(data_check, utility_table){
+
+  u_ids <- data_check |>
+    dplyr::filter(!c(utility_id %in% utility_table$utility_id)) |>
+    dplyr::select(utility, utility_id) |>
+    dplyr::distinct(utility, utility_id)
+
+  DT::datatable(u_ids)
+
+}
